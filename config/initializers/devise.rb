7ca9b1e2f3b223ab61c8d8e8a50d2f1c6a2eff71
Sidharth5117 <sidharth2136@gmail.com>
2020-04-29 19:31:03 +0530

    API keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-   config.omniauth :facebook, facebook_client_id,facebook_client_secret, scope: 'public_profile,email'
+   config.omniauth :facebook, ENV["facebook_client_id"],ENV["facebook_client_secret"], scope: 'public_profile,email'
    config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :google_client_id), Rails.application.credentials.dig(:google, :google_client_secret), scope: 'userinfo.email,userinfo.profile',skip_jwt: true
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
