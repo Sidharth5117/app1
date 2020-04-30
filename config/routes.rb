be2edc54598e11fb64bf402f6fa8af55de92a42b
Sidharth5117 <sidharth2136@gmail.com>
2020-04-30 12:33:05 +0530

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
-  as :user do
-root "devise/sessions#new"
-  end
+ devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
+
+
+
+
+root "home#about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
